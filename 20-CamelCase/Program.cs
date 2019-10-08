37f9a1ecf32e6a933f87efbcd11dfd758e6ff328
Mahmoud Ali <Mahmoud.ali@integrant.com>
2019-10-08 16:02:39 +0200

    Created algorithm to solve the problem of getting words from a string according to the camel case

---
+++ 20-CamelCase/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _20_CamelCase
+{
+    class Program
+    {
+        // Complete the camelcase function below.
+        static int camelcase(string s)
+        {
+            StringBuilder result = new StringBuilder("");
+            bool currentIsSmall = char.IsLower(s[0]);
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (currentIsSmall == char.IsLower(s[i]))
+                {
+                    result.Append(s[i]);
+                }
+                else
+                {
+                    result.Append(",").Append(s[i]);
+                    currentIsSmall = !char.IsLower(s[i]);
+                }
+            }
+            return result.ToString().Split(',').Count();
+        }
+
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int result = camelcase(s);
+            Console.WriteLine(result);
+
+        }
+    }
+}
