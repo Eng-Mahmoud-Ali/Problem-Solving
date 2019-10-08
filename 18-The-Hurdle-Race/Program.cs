7e1d1363d12b6b8190c694d9cd2a88b117663ff1
Mahmoud Ali <Mahmoud.ali@integrant.com>
2019-10-08 12:08:36 +0200

    Solve problem of Designer-PDF to get the integer power of characters

---
+++ 18-The-Hurdle-Race/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+namespace _18_The_Hurdle_Race
+{
+    class Program
+    {
+        // Complete the hurdleRace function below.
+        static int hurdleRace(int k, int[] height)
+        {
+            int max = height.Max();
+            if (k > max)
+                return 0;
+            return max - k; 
+        }
+
+        static void Main(string[] args)
+        {
+            string[] nk = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nk[0]);
+
+            int k = Convert.ToInt32(nk[1]);
+
+            int[] height = Array.ConvertAll(Console.ReadLine().Split(' '), heightTemp => Convert.ToInt32(heightTemp));
+            int result = hurdleRace(k, height);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 19-Designer-pdf-viewer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _19_Designer_pdf_viewer
+{
+    class Program
+    {
+        // Complete the designerPdfViewer function below.
+        static int designerPdfViewer(int[] h, string word)
+        {
+            // Complete this function
+            int temp = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                int ascii = (int)word.ElementAt(i);
+                int height = h[ascii - 97];
+                if (height > temp)
+                {
+                    temp = height;
+                }
+            }
+            return temp * word.Length;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] h = Array.ConvertAll(Console.ReadLine().Split(' '), hTemp => Convert.ToInt32(hTemp));
+            string word = Console.ReadLine();
+
+            int result = designerPdfViewer(h, word);
+
+            Console.WriteLine(result);
+        }
+    }
+}
